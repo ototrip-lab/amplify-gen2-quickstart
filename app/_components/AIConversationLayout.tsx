400974118ebe85c457261f6d3226ccf8286c8740
YujiAdachi <adachi389@mec-np.com>
2025-03-25 21:54:29 +0900

    chore : fix UI

--- app/_components/AIConversationLayout.tsx
+++ app/_components/AIConversationLayout.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import { View, useTheme } from "@aws-amplify/ui-react";
 import { AIConversation } from "@aws-amplify/ui-react-ai";
 import Markdown from "react-markdown";
 
 import { useAIConversation } from "@/app/client";
 
 export const AIConversationLayout = ({ id }: { id?: string }) => {
+  const { tokens } = useTheme();
   const [
     {
       data: { messages },
@@ -15,13 +17,15 @@ export const AIConversationLayout = ({ id }: { id?: string }) => {
   ] = useAIConversation("chat", { id });
 
   return (
-    <AIConversation
-      messages={messages}
-      isLoading={isLoading}
-      handleSendMessage={handleSendMessage}
-      messageRenderer={{
-        text: ({ text }) => <Markdown>{text}</Markdown>,
-      }}
-    />
+    <View padding={tokens.space.large}>
+      <AIConversation
+        messages={messages}
+        isLoading={isLoading}
+        handleSendMessage={handleSendMessage}
+        messageRenderer={{
+          text: ({ text }) => <Markdown>{text}</Markdown>,
+        }}
+      />
+    </View>
   );
 };
