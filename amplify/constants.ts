d53f623ee9334f7d3972162345e6c6f1adaa0b25
YujiAdachi <adachi389@mec-np.com>
2025-07-10 04:36:20 +0900

    chore : 予備のモデルを追加

--- amplify/constants.ts
+++ amplify/constants.ts
@@ -1,3 +1,4 @@
-export const BEDROCK_MODEL_PATH = "anthropic.claude-sonnet-4-20250514-v1:0";
-export const CROSS_REGION_NAME = "apac";
+export const BEDROCK_MODEL_PATH = 'anthropic.claude-sonnet-4-20250514-v1:0';
+// export const BEDROCK_MODEL_PATH = "amazon.nova-pro-v1:0";
+export const CROSS_REGION_NAME = 'apac';
 export const CROSS_REGION_BEDROCK_MODEL_PATH = `${CROSS_REGION_NAME}.${BEDROCK_MODEL_PATH}`;
