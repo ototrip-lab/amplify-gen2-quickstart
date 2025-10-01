4d934564acba7fbcd27d744c232eb34979dcb03a
YujiAdachi <adachi389@mec-np.com>
2025-10-01 20:06:46 +0900

    chore : lint

--- amplify/data/constants.ts
+++ amplify/data/constants.ts
@@ -1,3 +1,3 @@
-export const BEDROCK_MODEL_PATH = 'anthropic.claude-sonnet-4-5-20250929-v1:0';
-const CROSS_REGION_NAME = 'jp';
+export const BEDROCK_MODEL_PATH = "anthropic.claude-sonnet-4-5-20250929-v1:0";
+const CROSS_REGION_NAME = "jp";
 export const CROSS_REGION_BEDROCK_MODEL_PATH = `${CROSS_REGION_NAME}.${BEDROCK_MODEL_PATH}`;
